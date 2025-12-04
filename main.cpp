ad5e760a0398e088092de1a59af0b801a1c32da2
Nishat <nts7434@nyu.edu>
2025-12-04 13:04:53 -0500

    added heading to main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Nishat Shamiha
+* Assignment: Still Here
+* Date due: 12/05/25, 2:00pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "CS3113/ShaderProgram.h"
 
 // Global Constants
